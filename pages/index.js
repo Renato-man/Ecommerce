c79ae8d7127946dbbdb5098842c349ea751f0540
Renato Rodrigues <saever.vrko@gmail.com>
2022-05-04 16:21:56 +0200

    starting on components

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,22 @@ import React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 
-const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+      HeroBanner
+
+      <div className='products-heading'>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className='products-container'>
+        {['Product 1', 'Product 2'].map((product) => product)}
+      </div>
+
+      Footer
+    </>
   )
 }
 
-export default index
+export default Home
