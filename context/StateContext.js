470f98e899f400373f6b308915191862d9a5ece2
Renato Rodrigues <saever.vrko@gmail.com>
2022-05-18 11:45:32 +0200

    cart logic completed - starting with stripe payment

--- context/StateContext.js
+++ context/StateContext.js
@@ -6,14 +6,17 @@ const Context = createContext()
 export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false)
     const [cartItems, setcartItems] = useState([])
-    const [totalPrice, settotalPrice] = useState()
-    const [totalQuantities, settotalQuantities] = useState(0)
+    const [totalPrice, setTotalPrice] = useState(0)
+    const [totalQuantities, setTotalQuantities] = useState(0)
     const [qty, setqty] = useState(1)
+
+    let foundProduct;
+    let index;
 
     const onAdd = (product, quantity) => {
         const checkProductInCart = cartItems.find((item) => item._id === product._id)
-        settotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
-        settotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
 
         if (checkProductInCart) {
 
@@ -31,6 +34,34 @@ export const StateContext = ({ children }) => {
             setcartItems([...cartItems, { ...product }])
         }
         toast.success(`${qty} ${product.name} added to cart!`)
+    }
+
+    const onRemove = (product) => {
+        foundProduct = cartItems.find((item) => item._id === product._id)
+        const newCartItems = cartItems.filter((item) => item._id !== product._id)
+
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity)
+        setTotalQuantities(prevTotalQuantities => prevTotalQuantities - foundProduct.quantity)
+        setcartItems(newCartItems)
+    }
+
+    const toggleCartItemQuantity = (id, value) => {
+        foundProduct = cartItems.find((item) => item._id === id)
+        index = cartItems.findIndex((product) => product._id === id)
+        const newCartItems = cartItems.filter((item) => item._id !== id)
+
+        if (value === 'inc') {
+            setcartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }])
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+        } else if (value === 'dec') {
+            if (foundProduct.quantity > 1) {
+                setcartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 }])
+                setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+                setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+            }
+        }
+
     }
 
     const incQty = () => {
@@ -55,7 +86,9 @@ export const StateContext = ({ children }) => {
                 qty,
                 incQty,
                 decQty,
-                onAdd
+                onAdd,
+                toggleCartItemQuantity,
+                onRemove
             }}
         >
             {children}
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -55,7 +55,7 @@ const ProductDetails = ({ product, products }) => {
                     </div>
                     <div className="buttons">
                         <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
-                        <button type="button" className="buy-now" onClick=''>Buy Now</button>
+                        <button type="button" className="buy-now" >Buy Now</button>
                     </div>
                 </div>
             </div>
